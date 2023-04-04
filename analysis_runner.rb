21ba147aa67c7a4e7627dbd19d92d9a66690c7e5
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-04 15:15:59 -0400

    attempt to refactor

--- analysis_runner.rb
+++ analysis_runner.rb
@@ -4,3 +4,21 @@ require "./lib/vehicle_analysis"
 
 vehicle = Vehicle.new("pickup", true, true)
 VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("pickup", true, false)
+VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("pickup", false, false)
+VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("pickup", false, true)
+VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("car", true, true)
+VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("car", false, false)
+VehicleAnalysis.new.analyze(vehicle)
+
+vehicle = Vehicle.new("tractor", true, true)
+VehicleAnalysis.new.analyze(vehicle)
--- lib/vehicle_analysis.rb
+++ lib/vehicle_analysis.rb
@@ -3,22 +3,19 @@
 class VehicleAnalysis
 
   def analyze(vehicle)
+    puts "Vehicle has four wheels "
+
     if vehicle.car?
-      if vehicle.four_wheel_drive? || !vehicle.four_wheel_drive?
-        puts "Vehicle has four wheels "
         if vehicle.four_wheel_drive?
           puts "with four wheel drive"
         else
           puts "with two wheel drive"
         end
-      end
     elsif vehicle.tractor?
-      puts "Vehicle has four wheels "
       if vehicle.big_back_wheels?
         puts "with big wheels in the back"
       end
     elsif vehicle.pickup?
-      puts "Vehicle has four wheels "
       if vehicle.four_wheel_drive?
         puts "with four wheel drive"
       else
